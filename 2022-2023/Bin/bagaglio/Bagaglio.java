37d2413144f692d47c1f079b648dc475edda5dce
graziacobello <57944379+graziacobello@users.noreply.github.com>
2023-01-08 17:02:00 +0100

    Update Bagaglio.java

--- 2022-2023/Bin/bagaglio/Bagaglio.java
+++ 2022-2023/Bin/bagaglio/Bagaglio.java
@@ -7,15 +7,16 @@ public class Bagaglio {
         this.weight=0.0;
     }
     public Bagaglio(double w){
+        this.weight=0.0;
         setWeight(w);
     }
     public void setWeight(double w){
         if(w>0){
             this.weight=w;
-        }
+        }/*
         else{
-            this.weight=0;
-        }
+            this.weight=0.0;
+        }*/
     }
     public double getWeight(){
         return this.weight;
